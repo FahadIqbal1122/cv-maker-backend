8cc4eb9dc914270abd1bae136175aba59faf37d5
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-09-11 16:46:44 +0300

    Auth router create

---
+++ routes/AuthRouter.js
@@ -0,0 +1,6 @@
+const router = require("express").Router()
+const controller = require("../controllers/AuthController")
+
+router.post("/login", controller.Login)
+router.post("/register", controller.Register)
+module.exports = router
