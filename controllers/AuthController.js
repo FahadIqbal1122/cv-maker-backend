b3274f3ce121d6df2b5792fb580ff51cd68eb553
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-09-11 16:35:04 +0300

    create auth controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,22 @@
+const {User} = require('../models')
+
+const Login = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+module.exports = {
+  Login,
+  Register
+}
