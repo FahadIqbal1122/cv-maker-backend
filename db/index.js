5c5e9950e627ed635acd9a574afae2810a00cf1f
FahadIqbal1122 <fahadiqbal.fd14@gmail.com>
2024-09-11 16:44:32 +0300

    Create user schema

--- db/index.js
+++ db/index.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-  console.log('Successfully connected to MongoDB...')
-}).catch((e)=>{
-  console.error('Connection error', e.message)
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB...")
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message)
+  })
 
 const db = mongoose.connection
 
